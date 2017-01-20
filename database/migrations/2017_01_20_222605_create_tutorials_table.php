08b780e4f407373ecefb0509a413ff5ecda58825
Emran <emran.bm@gmail.com>
2017-01-21 02:00:23 +0330

    tutorials relation

---
+++ database/migrations/2017_01_20_222605_create_tutorials_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTutorialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tutorials', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('date');
+            $table->text('tutorial');
+            $table->integer('game_id')->unsigned();
+            $table->timestamps();
+        });
+
+        Schema::table('tutorials', function($table) {
+            $table->foreign('game_id')->references('id')->on('games');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tutorials');
+    }
+}
