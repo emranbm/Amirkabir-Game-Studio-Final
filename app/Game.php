692f67e642dd2fa632ba9d541340f640d3944c9a
Emran <emran.bm@gmail.com>
2017-01-20 22:25:16 +0330

    relations

--- app/Game.php
+++ app/Game.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Game extends Model
 {
-    //
+
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
+
+    public function records()
+    {
+        return $this->hasMany('App\Record');
+    }
 }
